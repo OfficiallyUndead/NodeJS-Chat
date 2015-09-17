ac238b778c5b5991faf843ddb625ab7da3d89ee9
d3ancole1995 <d3ancole1995@gmail.com>
2015-09-17 22:38:37 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res){
+  res.sendFile('/home/d3ancole1995/Web/index.html');
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+io.on('connection', function(socket){
+  socket.on('chat message', function(user,msg){
+    console.log('message: ' + user + ' - ' + msg);
+    io.emit('chat message', user,msg);
+  });
+});
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
